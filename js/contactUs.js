04661f4f69cf9db03025879bfdfd2b01aad09efb
MartEvgeny <evgproj@gmail.com>
2021-11-07 14:59:34 +0200

    fixing contact us position

--- js/contactUs.js
+++ js/contactUs.js
@@ -1,16 +1,15 @@
 function submitMessage() {
+  const mainPage = document.getElementById("main");
   const comment = document.getElementById("box-2");
   let firstName = document.querySelector("#firstName").value;
   let secondName = document.querySelector("#secondName").value;
-  // let backToTheMainPage = document.createElement("a");
-  // backToTheMainPage.text = "Back to the main page";
-  // backToTheMainPage.href = "index.html";
-  comment.innerHTML = `Hi ${firstName} ${secondName},
-   Thank you for contacting us, we will call you soon.`;
-  comment.style.width = "fit-content";
-  comment.style.backgroundColor = "rgb(34, 169, 199)";
-  comment.style.color = "white";
-  //comment.appendChild(backToTheMainPage);
+  mainPage.innerHTML = `Hi ${firstName} ${secondName},
+   Thank you for reaching out, we will call you soon.`;
+  mainPage.style.width = "fit-content";
+  mainPage.style.backgroundColor = "rgb(34, 169, 199)";
+  mainPage.style.color = "white";
+  mainPage.style.marginTop = "15px";
+  mainPage.style.marginLeft = "5px";
   window.setTimeout(function () {
     window.location.href = "index.html";
   }, 4000);
